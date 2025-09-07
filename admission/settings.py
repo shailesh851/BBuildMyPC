5ae431f7069f5089effc04958dbd1d713d757d34
shailesh <sk@gmail.com>
2025-09-08 01:15:28 +0530

    fo2

--- admission/settings.py
+++ admission/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(((kp%6w19d&wxm5wp!71@7shce_2-5=ysg&*^+bz65rszp_3h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
