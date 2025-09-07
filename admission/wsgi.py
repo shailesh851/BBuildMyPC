618d5c4a087c3de63913fb9a68d7abc42776c254
shailesh <sk@gmail.com>
2025-09-08 00:44:18 +0530

    fo

--- admission/wsgi.py
+++ admission/wsgi.py
@@ -10,6 +10,6 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BuildMyPCbackend.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "admission.settings")
 application = get_wsgi_application()
 
