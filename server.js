9f07da5538a1718ff5df14541a7caf073df32060
shailesh <sk@gmail.com>
2025-09-17 14:13:24 +0530

    122

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const dotenv =require("dotenv");
 dotenv.config();
 
 app.use(cors({
-  origin: "http://localhost:3000", 
+  origin: "https://bbuildmypc.onrender.com", 
   credentials: true               
 }));
 
